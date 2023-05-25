df40d3178de2ceb13e885e76dc9af9c2e795b264
Aline LEGENDRE <legendrealine@gmail.com>
2023-05-25 14:23:56 +0200

    quete 14

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -13,22 +13,27 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
     public const PROGRAMS = [
         ['title' => 'Walking dead',
             'synopsis' => 'Des zombies envahissent la terre',
+            'poster' =>"asset('build/walkingdead.jpg')",
             'category' => 'Action',
             ],
         ['title' => 'Game of Thrones',
             'synopsis' => "Sur le continent de Westeros, en proie au retour de l'Hiver, on se disputent le Trône de Fer.",
+            'poster' => "asset('build/gameofthrones.jpg')",
             'category' => 'Fantastique',
             ],
         ['title' => 'The Last of Us',
             'synopsis' => "Une pandémie provoquée par le cordyceps à décimée l'humanité.",
+            'poster' => "asset('build/thelastofus.jpeg')",
             'category' => 'Action',
         ],
         ['title' => 'The 100',
             'synopsis' => "Après une apocalypse nucléaire les réfugiés des stations spatiales retournent sur Terre.",
+            'poster' => "asset('build/the100.webp')",
             'category' => 'Action',
             ],
         ['title' => 'The Haunting of Bly Manor',
             'synopsis' => "Une institutrice est engagée pour veiller sur deux orphelins vivant dans un manoir isolé.",
+            'poster' => "asset('build/thehautingofblymanor.jpeg')",
             'category' => "Horreur",
             ],
     ];
@@ -40,6 +45,7 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
             $program = new Program();
             $program->setTitle($sousProgram['title']);
             $program->setSynopsis($sousProgram['synopsis']);
+            $program->setPoster($sousProgram['poster']);
             $program->setCategory($this->getReference('category_' . $sousProgram['category']));
             $this->addReference('program_' . str_replace(' ','',  $sousProgram['title']), $program);
             $manager->persist($program);
