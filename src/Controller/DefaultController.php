002018281b69e352752adf8a0ac42a7f43c23c62
Aline LEGENDRE <legendrealine@gmail.com>
2023-06-13 17:19:41 +0200

    quete 29

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,32 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Form\SearchProgramType;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(ProgramRepository $programRepository): Response
+    public function index(ProgramRepository $programRepository, Request $request): Response
     {
-        $programs = $programRepository->findBy([], ['id' => 'DESC']);
+
+        $form = $this->createForm(SearchProgramType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['Recherche'];
+            $programs = $programRepository->findLikeNameAndActor($search);
+        } else {
+            $programs = $programRepository->findBy([], ['id' => 'DESC']);
+        }
 
         return $this->render('index.html.twig', [
             'programs' => $programs,
+            'form' => $form,
         ]);
     }
 }
